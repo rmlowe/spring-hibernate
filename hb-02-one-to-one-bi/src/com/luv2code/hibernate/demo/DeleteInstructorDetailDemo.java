9862acf5ad9d7b79ca2e2ad31aeb74c923878979
Robert Lowe <robertmlowe@rmlowe.com>
2018-05-21 09:05:03 +0800

    @OneToOne - Bi-Directional - Delete Only InstructorDetail - Part 1

--- hb-02-one-to-one-bi/src/com/luv2code/hibernate/demo/DeleteInstructorDetailDemo.java
+++ hb-02-one-to-one-bi/src/com/luv2code/hibernate/demo/DeleteInstructorDetailDemo.java
@@ -27,7 +27,7 @@ public class DeleteInstructorDetailDemo {
 			session.beginTransaction();
 
 			// get the instructor detail object
-			int theId = 2;
+			int theId = 3;
 			InstructorDetail tempInstructorDetail =
 					session.get(InstructorDetail.class, theId);
 			
