46ffa56b4a65526a3325d59d710832ac46a4eb20
Robert Lowe <robertmlowe@rmlowe.com>
2018-07-01 21:02:16 +0800

    AOP: @Before Advice - Testing AOP Aspect

--- spring-demo-aop/src/com/luv2code/aopdemo/MainDemoApp.java
+++ spring-demo-aop/src/com/luv2code/aopdemo/MainDemoApp.java
@@ -18,6 +18,12 @@ public class MainDemoApp {
 		// call the business method
 		theAccountDAO.addAccount();
 		
+		// do it again!
+		System.out.println("\n let's call it again!\n");
+		
+		// call the business method again
+		theAccountDAO.addAccount();
+		
 		// close the context
 		context.close();
 	}
