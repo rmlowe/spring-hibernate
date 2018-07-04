3403c37382d3af6e6d2fbb163035812786fc68b1
Robert Lowe <robertmlowe@rmlowe.com>
2018-07-05 01:16:47 +0800

    AOP: @AfterReturning - Modifying Data - Write Some Code

--- spring-demo-aop-z-after-returning/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop-z-after-returning/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -32,9 +32,34 @@ public class MyDemoLoggingAspect {
 		// print out the results of the method call
 		System.out.println("\n=====>>> result is: " + result);
 		
+		// let's post-process the data ... let's modify it :-)
+		
+		// convert the account names to uppercase
+		convertAccountNamesToUppercase(result);
+		
+		System.out.println("\n=====>>> result is: " + result);
+		
 	}
 	
+	private void convertAccountNamesToUppercase(List<Account> result) {
+
+		// loop through accounts
+		
+		for (Account tempAccount : result) {
+			
+			// get uppercase version of name
+			String theUpperName = tempAccount.getName().toUpperCase();
+			
+			// update the name on the account
+			tempAccount.setName(theUpperName);
+		}
+		
+	}
+
 	
+	
+	
+
 
 	@Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
 	public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
