847aa1c5b38e81b69b58fafa27af087daa6620fe
Robert Lowe <robertmlowe@rmlowe.com>
2018-07-07 10:25:23 +0800

    AOP: @Around - Write Some Code - Part 2

--- spring-demo-aop-zz-around/src/com/luv2code/aopdemo/service/TrafficFortuneService.java
+++ spring-demo-aop-zz-around/src/com/luv2code/aopdemo/service/TrafficFortuneService.java
@@ -2,6 +2,9 @@ package com.luv2code.aopdemo.service;
 
 import java.util.concurrent.TimeUnit;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class TrafficFortuneService {
 
 	public String getFortune() {
