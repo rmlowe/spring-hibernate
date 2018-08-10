b86a728f08ea2d998b302a482f5d27e0bedc9327
Robert Lowe <robertmlowe@rmlowe.com>
2018-08-10 20:06:38 +0800

    Spring Security - Bootstrap CSS Login Form - Testing

--- spring-security-demo-04-bootstrap-login-fom/src/main/java/com/luv2code/springsecurity/demo/controller/LoginController.java
+++ spring-security-demo-04-bootstrap-login-fom/src/main/java/com/luv2code/springsecurity/demo/controller/LoginController.java
@@ -9,7 +9,9 @@ public class LoginController {
 	@GetMapping("/showMyLoginPage")
 	public String showMyLoginPage() {
 		
-		return "plain-login";
+		// return "plain-login";
+		
+		return "fancy-login";
 		
 	}
 }
