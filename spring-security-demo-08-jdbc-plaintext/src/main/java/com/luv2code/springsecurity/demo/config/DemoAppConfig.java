9cfe28255a214b614019a7b5b83be592e97c618e
Robert Lowe <robertmlowe@rmlowe.com>
2018-08-11 04:59:25 +0800

    Spring Security JDBC - Coding - Define DataSource

--- spring-security-demo-08-jdbc-plaintext/src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
+++ spring-security-demo-08-jdbc-plaintext/src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
@@ -1,8 +1,15 @@
 package com.luv2code.springsecurity.demo.config;
 
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -10,8 +17,19 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="com.luv2code.springsecurity.demo")
+@PropertySource("classpath:persistence-mysql.properties")
 public class DemoAppConfig {
 
+	// set up variable to hold the properties
+	
+	@Autowired
+	private Environment env;
+	
+	// set up a logger for diagnostics
+	
+	private Logger logger = Logger.getLogger(getClass().getName());
+	
+	
 	// define a bean for ViewResolver
 	
 	@Bean
@@ -25,4 +43,21 @@ public class DemoAppConfig {
 		return viewResolver;
 	}
 	
+	// define a bean for our security datasource
+	
+	@Bean
+	public DataSource securityDataSource() {
+		
+		// create connection pool
+		
+		// set the jdbc driver class
+		
+		// log the connection props
+		
+		// set database connection props
+		
+		// set connection pool props
+		
+		return null;
+	}
 }
