b0621f62960825089c5f31c07a7d113f422de0a8
Robert Lowe <robertmlowe@rmlowe.com>
2019-01-10 14:18:42 +0800

    Spring Boot - Spring Data REST - Configuration, Pagination and Sorting

--- 24-spring-data-rest-cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ 24-spring-data-rest-cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 
+// @RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 	
 	// that's it ... no need to write any code LOL!
