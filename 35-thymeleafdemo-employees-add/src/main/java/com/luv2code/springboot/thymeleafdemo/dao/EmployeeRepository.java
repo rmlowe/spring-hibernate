3f93e143e3c344e3b4d8ac30bd4502a7cca3f8db
Robert Lowe <robertmlowe@rmlowe.com>
2019-02-14 23:59:23 +0000

    Spring Boot - Thymeleaf - CRUD Database Real-Time Project: Add Employee - Part 3

--- 35-thymeleafdemo-employees-add/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ 35-thymeleafdemo-employees-add/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package com.luv2code.springboot.thymeleafdemo.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
@@ -8,4 +10,7 @@ public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 	
 	// that's it ... no need to write any code LOL!
 
+	// add a method to sort by last name
+	public List<Employee> findAllByOrderByLastNameAsc();
+	
 }
